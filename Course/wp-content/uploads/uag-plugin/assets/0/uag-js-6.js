6817d5ddb7faa9cd5dc1153a77c140c864083974
Tiago Fernandes <129887396+tiagofernandes3@users.noreply.github.com>
2024-12-20 23:41:28 -0300

    Add files via upload

---
+++ Course/wp-content/uploads/uag-plugin/assets/0/uag-js-6.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-c214eb7b' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-c214eb7b' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-c214eb7b' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-c214eb7b' );
+});
+ });
